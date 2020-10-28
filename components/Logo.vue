2e758becc73f57c5b9bf6765b864564001d3a864
Kevin Olson <acidjazz@gmail.com>
2020-10-28 02:18:54 -0500

    🎉 initial commit

---
+++ components/Logo.vue
@@ -0,0 +1,18 @@
+<template>
+  <img width="40%" src="https://i2.wp.com/coopcancook.com/wp-content/uploads/2019/08/Photo-Jun-30-1-40-02-PM.jpg?fit=1736%2C2408&ssl=1" />
+</template>
+
+<style>
+img {
+  animation: 1s appear;
+  margin: auto;
+  with: 60%;
+  height: 60%;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
